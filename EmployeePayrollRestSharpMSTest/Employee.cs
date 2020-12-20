48bf5b7ef38a56641def63cf14f31fc9daf501de
“teji1998” <tejasvinirpk@gmail.com>
2020-12-21 02:14:48 +0530

    Added:POCO class for Employee

---
+++ EmployeePayrollRestSharpMSTest/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollRestSharpMSTest
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+}
